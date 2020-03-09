7436be234b979e0bd30f51b3787c6228df752489
Luiz Otavio <luizotaviomartinsf@gmail.com>
2020-03-09 13:14:30 -0300

    Commit 1- Criar Projeto Api Rest Agenda

---
+++ src/test/java/com/br/api/agenda/ApiAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.api.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
